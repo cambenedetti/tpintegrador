dafd8b0432bf96b56ba50fb5ec7d7623e7e0a0c7
cambendetti <claudioambenedetti@gmail.com>
2023-06-18 00:58:21 -0300

    Trabajo práctico integrador JS

---
+++ script.js
@@ -0,0 +1,36 @@
+/*
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+*/
+
+const cantidadEntradas = document.getElementById('cantidadEntradas')
+
+const botonCalcular = document.getElementById('botonCalcular')
+
+const categoria = document.getElementById('inputState')
+
+const totalCompra = document.getElementById('totalCompra')
+
+function resumen(){
+
+    console.log(categoria.value)
+
+    console.log(cantidadEntradas.value)
+
+    cantidadEntradas.value
+
+if(categoria.value == "estudiante"){
+    var valorDescuento = 200 / 100 * 20  
+    var valorTotal = valorDescuento * Number(cantidadEntradas.value)  
+}
+if(categoria.value == "trainee"){
+    var valorDescuento = 200 / 100 * 50  
+    var valorTotal = valorDescuento * Number(cantidadEntradas.value)  
+}
+if(categoria.value == "junior"){
+    var valorDescuento = 200 / 100 * 85  
+    var valorTotal = valorDescuento * Number(cantidadEntradas.value)  
+}
+    totalCompra.innerHTML = "$" + valorTotal
+}
